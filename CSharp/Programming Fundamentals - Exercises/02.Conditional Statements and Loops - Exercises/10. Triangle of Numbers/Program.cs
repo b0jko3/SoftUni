eb4b7769fae5e30cc745f5b108f6b9b3f7678046
b0jko3 <bozhidar.kayryakov@abv.bg>
2017-09-26 17:53:46 +0300

    10

---
+++ CSharp/Programming Fundamentals - Exercises/02.Conditional Statements and Loops - Exercises/10. Triangle of Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Triangle_of_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                Console.Write(i);
+
+                for (int j = 0; j < i - 1; j++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
