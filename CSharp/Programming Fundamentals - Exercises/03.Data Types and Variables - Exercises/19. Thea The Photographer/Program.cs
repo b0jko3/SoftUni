5c3c7e82c64bb1a1ce62917808fdd07b2ab8fa66
b0jko3 <bozhidar.kayryakov@abv.bg>
2017-09-29 21:35:15 +0300

    19 with time limit

---
+++ CSharp/Programming Fundamentals - Exercises/03.Data Types and Variables - Exercises/19. Thea The Photographer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19.Thea_The_Photographer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal numberOfPictures = decimal.Parse(Console.ReadLine());
+            decimal secondsToFilter = decimal.Parse(Console.ReadLine());
+            decimal percentageGoodPictures = decimal.Parse(Console.ReadLine());
+            decimal secondsToUpload = decimal.Parse(Console.ReadLine());
+            decimal goodPictures = numberOfPictures - (numberOfPictures - numberOfPictures * percentageGoodPictures / 100);
+            Console.WriteLine(goodPictures);
+            decimal seconds = (numberOfPictures * secondsToFilter) + (Math.Ceiling(goodPictures) * secondsToUpload);
+            Console.WriteLine(seconds);
+            decimal minutes = 0;
+            decimal hours = 0;
+            decimal days = 0;
+
+            while(seconds > 59)
+            {
+                seconds -= 60;
+                minutes++;
+            }
+            while (minutes > 59)
+            {
+                minutes -= 60;
+                hours++;
+            }
+            while (hours > 23)
+            {
+                hours -= 24;
+                days++;
+            }
+            Console.WriteLine($"{(int)days}:{(int)hours:d2}:{(int)minutes:d2}:{(int)seconds:d2}");
+        }
+    }
+}
