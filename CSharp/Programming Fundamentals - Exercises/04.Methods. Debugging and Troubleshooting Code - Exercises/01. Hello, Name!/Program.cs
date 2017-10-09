62388b93bec5fd43e621390112cebb4186a96fd5
b0jko3 <bozhidar.kayryakov@abv.bg>
2017-10-09 17:12:22 +0300

    methods hw

---
+++ CSharp/Programming Fundamentals - Exercises/04.Methods. Debugging and Troubleshooting Code - Exercises/01. Hello, Name!/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Hello__Name_
+{
+    class Program
+    {
+        public static string PrintName(string name)
+        {
+            string result = $"Hello, {name}!";
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            
+            Console.WriteLine(PrintName(name));
+        }
+    }
+}
---
+++ CSharp/Programming Fundamentals - Exercises/04.Methods. Debugging and Troubleshooting Code - Exercises/02. Max Method/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Max_Method
+{
+    class Program
+    {
+        public static int GetMax(int firstNumber, int secondNumber)
+        {
+            int result = 0;
+
+            if (firstNumber >= secondNumber)
+            {
+                result = firstNumber;
+            }
+            else
+            {
+                result = secondNumber;
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetMax(GetMax(firstNumber, secondNumber), thirdNumber));
+        }
+    }
+}
