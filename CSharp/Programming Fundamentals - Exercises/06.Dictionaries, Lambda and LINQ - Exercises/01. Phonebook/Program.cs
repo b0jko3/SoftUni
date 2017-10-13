a262c7c6dd39cb778a68dcc676284fced191d8a8
b0jko3 <bozhidar.kayryakov@abv.bg>
2017-10-13 21:54:42 +0300

    dict hw 01

---
+++ CSharp/Programming Fundamentals - Exercises/06.Dictionaries, Lambda and LINQ - Exercises/01. Phonebook/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Phonebook
+{
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<string, string> phoneBook = new Dictionary<string, string>();
+            string end = "END";
+            string inputString = "";
+
+            while (inputString != end)
+            {
+                inputString = Console.ReadLine();
+
+                if (inputString[0] == 'A')
+                {
+                    inputString = inputString.Remove(0, 2);
+                    phoneBook[inputString.Substring(0, inputString.IndexOf(' '))] = inputString.Substring(inputString.IndexOf(' '));
+                }
+                else if (inputString[0] == 'S')
+                {
+                    inputString = inputString.Remove(0, 2);
+                    if (phoneBook.ContainsKey(inputString.Substring(0)))
+                    {
+                        Console.WriteLine($"{inputString.Substring(0)} ->{phoneBook[inputString.Substring(0)]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Contact {inputString.Substring(0)} does not exist.");
+                    }
+                }
+                else if (inputString == end)
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
