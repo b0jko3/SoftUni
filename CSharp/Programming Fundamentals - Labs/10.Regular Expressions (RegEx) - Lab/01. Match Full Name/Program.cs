95d00eb8c71621bb3880eac07f7a542978eb3dbf
b0jko3 <bozhidar.kayryakov@abv.bg>
2017-10-26 17:18:26 +0300

    RegEx Lab 01

---
+++ CSharp/Programming Fundamentals - Labs/10.Regular Expressions (RegEx) - Lab/01. Match Full Name/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _01.Match_Full_Name
+{
+    class Program
+    {
+        static void Main()
+        {
+            string text = Console.ReadLine();
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+
+            MatchCollection names = Regex.Matches(text, pattern);
+
+            foreach (Match name in names)
+            {
+                Console.Write(name.Value + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ CSharp/Programming Fundamentals - Labs/10.Regular Expressions (RegEx) - Lab/02. Match Phone Number/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Match_Phone_Number
+{
+    class Program
+    {
+        static void Main()
+        {
+            string text = Console.ReadLine();
+            string pattern = @"\+359 2 \d{3} \d{4}\b|\+359-2-\d{3}-\d{4}\b";
+
+
+        }
+    }
+}
