d394308ef704a948438db921b9995e2c8985fdb8
b0jko3 <bozhidar.kayryakov@abv.bg>
2017-10-27 22:00:39 +0300

    RegEx exercises 01 and 02 80/100

---
+++ CSharp/Programming Fundamentals - Exercises/10.Regular Expressions (RegEx) - Exercises/01. Extract Emails/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _01.Extract_Emails
+{
+    class Program
+    {
+        static void Main()
+        {
+            string inputText = Console.ReadLine();
+            string pattern = @"\s[0-9A-Za-z]+([.-]\w*)*@(\w+[.-])*(\w+[.-]\w+)+";
+
+            MatchCollection emails = Regex.Matches(inputText, pattern);
+
+            foreach (Match email in emails)
+            {
+                Console.WriteLine(email.Value.Trim());
+            }
+        }
+    }
+}
---
+++ CSharp/Programming Fundamentals - Exercises/10.Regular Expressions (RegEx) - Exercises/02. Extract Sentences by Keyword/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Extract_Sentences_by_Keyword
+{
+    class Program
+    {
+        static void Main()
+        {
+            string word = Console.ReadLine();
+            string[] text = Console.ReadLine().Split(new char[] { '.', '?', '!' }, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i].Contains(" " + word + " "))
+                {
+                    Console.WriteLine(text[i].Trim());
+                }
+            }
+        }
+    }
+}
