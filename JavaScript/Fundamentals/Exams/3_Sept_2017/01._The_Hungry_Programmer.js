d348025e8bfd205c83f6618317ee7558abdedd0d
b0jko3 <bozhidar.kayryakov@abv.bg>
2018-02-06 16:50:20 +0200

    exams to be continue

---
+++ JavaScript/Fundamentals/Exams/3_Sept_2017/01._The_Hungry_Programmer.js
@@ -0,0 +1,19 @@
+function theHungryProgrammer(meals, commands) {
+    //To be continue
+}
+
+theHungryProgrammer(['chicken', 'steak', 'eggs'], ['Serve', 'Eat', 'End', 'Consume 0 1'])
+// eggs served!
+// chicken eaten
+// Meals left: steak
+// Meals eaten: 1
+
+//theHungryProgrammer(['fries', 'fish', 'beer', 'chicken', 'beer', 'eggs'], ['Add spaghetti', 'Shift 0 1', 'Consume 1 4', 'End'])
+// Burp!
+// Meals left: fries, beer, eggs
+// Meals eaten: 4
+
+//theHungryProgrammer(['carrots', 'apple', 'beet'], ['Consume 0 2', 'End',])
+// Burp!
+// The food is gone
+// Meals eaten: 3
