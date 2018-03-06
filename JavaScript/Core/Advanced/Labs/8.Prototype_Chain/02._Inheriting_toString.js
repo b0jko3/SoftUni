2e83eba52045899a75e6607a89029a1ff1a9feeb
b0jko3 <bozhidar.kayryakov@abv.bg>
2018-03-06 16:47:07 +0200

    labs

---
+++ JavaScript/Core/Advanced/Labs/8.Prototype_Chain/02._Inheriting_toString.js
@@ -0,0 +1,45 @@
+function inherittingToString() {
+    class Person {
+        constructor(name, email) {
+            this.name = name
+            this.email = email
+        }
+
+        toString() {
+            let className = this.constructor.name
+            return `${className} (name: ${this.name}, email: ${this.email})`
+        }
+    }
+
+    class Teacher extends Person {
+        constructor(name, email, subject) {
+            super(name, email)
+            this.subject = subject
+        }
+
+        toString() {
+            let className = this.constructor.name
+            return `${className} (name: ${this.name}, email: ${this.email}, subject: ${this.subject})`
+        }
+    }
+
+    class Student extends Person {
+        constructor(name, email, course) {
+            super(name, email)
+            this.course = course
+        }
+
+        toString() {
+            let className = this.constructor.name
+            return `${className} (name: ${this.name}, email: ${this.email}, course: ${this.course})`
+        }
+    }
+
+    return {
+        Person,
+        Teacher,
+        Student
+    }
+}
+
+console.log(inherittingToString())
