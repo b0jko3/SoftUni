72bb75e7c46b284eb0e84c5aecb90fa9eae7e399
b0jko3 <bozhidar.kayryakov@abv.bg>
2018-03-12 21:17:33 +0200

    exam prep 1

---
+++ JavaScript/Core/Advanced/Exams/23_July_2017/01._Select_and_Print/move.js
@@ -0,0 +1,11 @@
+function move(command) {
+    // let availableTownsList = document.getElementById('available-towns')
+    // let selectedTownsList = document.getElementById('selected-towns')
+
+    // to do
+  }
+
+
+// "right" is received when the  button is clicked – move the selected town from Available towns to Selected towns
+// "left" is received when the  button is clicked – move the selected town from Selected towns to Available towns
+// "print" – Replace the contents of the div element with ID "output" with a list of the town names of all towns from Selected towns, joined with a semicolon and space ("; ").
